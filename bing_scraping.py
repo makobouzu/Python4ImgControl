fede7d296f21b530cafc0f42c46037f1d20f9ba7
makobouzu <amako20@iamas.ac.jp>
2020-07-12 00:40:26 +0900

    add makedir

--- bing_scraping.py
+++ bing_scraping.py
@@ -1,11 +1,16 @@
 #pip install icrawler
 #python bing_scraping.py filename keyword num
+import os
 import sys
 from icrawler.builtin import BingImageCrawler
 
 if __name__ == '__main__':
+    path = 'root_dir' + 'bing'
+    if not os.path.isdir(path):
+        os.makedirs(path)
+
     args = sys.argv
-    crawler = BingImageCrawler(storage={"root_dir": str(args[1])})
+    crawler = BingImageCrawler(storage={'root_dir': 'bing' + '/' + str(args[1])})
     filters = {
         'size': 'large',
         'type': 'photo'
